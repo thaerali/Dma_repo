e021d140654436fbe97c24a485871a6c6311c979
Hussien23 <hussienalisy997@outlook.sa>
2019-05-01 21:18:00 -0800

    hello from Hussien

---
+++ four.java
@@ -0,0 +1,12 @@
+public class Four
+{ 
+	public void print()
+	{
+		System.out.println("Java");	
+	}
+
+	public void printHussien_HA()
+	{
+	System.out.println("Java from Hussien Ali Al-Haj Ali");
+	}
+}
